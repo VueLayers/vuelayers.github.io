8f4d428ba0443d9952ab2f918930172f818d9e04
ghettovoice <ghettovoice@gmail.com>
2017-09-30 17:01:00 +0300

    update doc site

---
+++ static/js/manifest.3438ce99e27c1e6ae25d.js
@@ -0,0 +1,12 @@
+/*!
+VueLayers
+Vue 2 components to work with OpenLayers
+
+@package vuelayers
+@author Vladimir Vershinin <ghettovoice@gmail.com>
+@version 0.7.1
+@license MIT
+@copyright (c) 2017, Vladimir Vershinin <ghettovoice@gmail.com>
+*/
+!function(e){function n(r){if(t[r])return t[r].exports;var o=t[r]={i:r,l:!1,exports:{}};return e[r].call(o.exports,o,o.exports,n),o.l=!0,o.exports}var r=window.webpackJsonp;window.webpackJsonp=function(t,a,c){for(var i,u,f,s=0,d=[];s<t.length;s++)u=t[s],o[u]&&d.push(o[u][0]),o[u]=0;for(i in a)Object.prototype.hasOwnProperty.call(a,i)&&(e[i]=a[i]);for(r&&r(t,a,c);d.length;)d.shift()();if(c)for(s=0;s<c.length;s++)f=n(n.s=c[s]);return f};var t={},o={8:0};n.e=function(e){function r(){i.onerror=i.onload=null,clearTimeout(u);var n=o[e];0!==n&&(n&&n[1](new Error("Loading chunk "+e+" failed.")),o[e]=void 0)}var t=o[e];if(0===t)return new Promise(function(e){e()});if(t)return t[2];var a=new Promise(function(n,r){t=o[e]=[n,r]});t[2]=a;var c=document.getElementsByTagName("head")[0],i=document.createElement("script");i.type="text/javascript",i.charset="utf-8",i.async=!0,i.timeout=12e4,n.nc&&i.setAttribute("nonce",n.nc),i.src=n.p+"static/js/"+e+"."+{0:"7a6ffb9aff812052fa8d",1:"6e86479c701062000763",2:"32509a90153b51c180d7",3:"d347cb39ee958b20d769",4:"a47a0d258bb0adc12c7e",5:"0bab251bae3c76875999",6:"e539b946379b061bc5d6",7:"56ab52d826458b5bd824"}[e]+".js";var u=setTimeout(r,12e4);return i.onerror=i.onload=r,c.appendChild(i),a},n.m=e,n.c=t,n.d=function(e,r,t){n.o(e,r)||Object.defineProperty(e,r,{configurable:!1,enumerable:!0,get:t})},n.n=function(e){var r=e&&e.__esModule?function(){return e.default}:function(){return e};return n.d(r,"a",r),r},n.o=function(e,n){return Object.prototype.hasOwnProperty.call(e,n)},n.p="/",n.oe=function(e){throw console.error(e),e}}([]);
+//# sourceMappingURL=manifest.3438ce99e27c1e6ae25d.js.map
