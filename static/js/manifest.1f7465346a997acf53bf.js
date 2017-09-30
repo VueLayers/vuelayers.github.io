b63b77d1b5c58efa9d0e041050f8f9f38ff2f916
ghettovoice <ghettovoice@gmail.com>
2017-09-30 14:06:41 +0300

    update doc site

---
+++ static/js/manifest.1f7465346a997acf53bf.js
@@ -0,0 +1,12 @@
+/*!
+VueLayers
+Vue 2 components to work with OpenLayers
+
+@package vuelayers
+@author Vladimir Vershinin <ghettovoice@gmail.com>
+@version 0.7.1
+@license MIT
+@copyright (c) 2017, Vladimir Vershinin <ghettovoice@gmail.com>
+*/
+!function(e){function n(r){if(t[r])return t[r].exports;var o=t[r]={i:r,l:!1,exports:{}};return e[r].call(o.exports,o,o.exports,n),o.l=!0,o.exports}var r=window.webpackJsonp;window.webpackJsonp=function(t,c,a){for(var i,u,f,d=0,s=[];d<t.length;d++)u=t[d],o[u]&&s.push(o[u][0]),o[u]=0;for(i in c)Object.prototype.hasOwnProperty.call(c,i)&&(e[i]=c[i]);for(r&&r(t,c,a);s.length;)s.shift()();if(a)for(d=0;d<a.length;d++)f=n(n.s=a[d]);return f};var t={},o={8:0};n.e=function(e){function r(){i.onerror=i.onload=null,clearTimeout(u);var n=o[e];0!==n&&(n&&n[1](new Error("Loading chunk "+e+" failed.")),o[e]=void 0)}var t=o[e];if(0===t)return new Promise(function(e){e()});if(t)return t[2];var c=new Promise(function(n,r){t=o[e]=[n,r]});t[2]=c;var a=document.getElementsByTagName("head")[0],i=document.createElement("script");i.type="text/javascript",i.charset="utf-8",i.async=!0,i.timeout=12e4,n.nc&&i.setAttribute("nonce",n.nc),i.src=n.p+"static/js/"+e+"."+{0:"62c118906fa72405a7cd",1:"c07fabada99d8374051a",2:"ac6d8bc0e4e1f528ea16",3:"6e46b2f6d63c27bc4643",4:"75d7010c782b0f103d54",5:"b5bfa948db892c4688ba",6:"e539b946379b061bc5d6",7:"b64c15a78724fe4225d5"}[e]+".js";var u=setTimeout(r,12e4);return i.onerror=i.onload=r,a.appendChild(i),c},n.m=e,n.c=t,n.d=function(e,r,t){n.o(e,r)||Object.defineProperty(e,r,{configurable:!1,enumerable:!0,get:t})},n.n=function(e){var r=e&&e.__esModule?function(){return e.default}:function(){return e};return n.d(r,"a",r),r},n.o=function(e,n){return Object.prototype.hasOwnProperty.call(e,n)},n.p="/",n.oe=function(e){throw console.error(e),e}}([]);
+//# sourceMappingURL=manifest.1f7465346a997acf53bf.js.map
