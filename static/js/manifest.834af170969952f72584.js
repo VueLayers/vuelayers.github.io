6c23600638fb5246c24a25ed3d5e174eb3dcac20
ghettovoice <ghettovoice@gmail.com>
2017-12-30 17:47:38 +0300

    update doc site

---
+++ static/js/manifest.834af170969952f72584.js
@@ -0,0 +1,12 @@
+/*!
+VueLayers
+Vue 2 components to work with OpenLayers
+
+@package vuelayers
+@author Vladimir Vershinin <ghettovoice@gmail.com>
+@version 0.8.0
+@license MIT
+@copyright (c) 2017, Vladimir Vershinin <ghettovoice@gmail.com>
+*/
+!function(e){function n(r){if(t[r])return t[r].exports;var o=t[r]={i:r,l:!1,exports:{}};return e[r].call(o.exports,o,o.exports,n),o.l=!0,o.exports}var r=window.webpackJsonp;window.webpackJsonp=function(t,a,c){for(var i,u,d,f=0,s=[];f<t.length;f++)u=t[f],o[u]&&s.push(o[u][0]),o[u]=0;for(i in a)Object.prototype.hasOwnProperty.call(a,i)&&(e[i]=a[i]);for(r&&r(t,a,c);s.length;)s.shift()();if(c)for(f=0;f<c.length;f++)d=n(n.s=c[f]);return d};var t={},o={9:0};n.e=function(e){function r(){i.onerror=i.onload=null,clearTimeout(u);var n=o[e];0!==n&&(n&&n[1](new Error("Loading chunk "+e+" failed.")),o[e]=void 0)}var t=o[e];if(0===t)return new Promise(function(e){e()});if(t)return t[2];var a=new Promise(function(n,r){t=o[e]=[n,r]});t[2]=a;var c=document.getElementsByTagName("head")[0],i=document.createElement("script");i.type="text/javascript",i.charset="utf-8",i.async=!0,i.timeout=12e4,n.nc&&i.setAttribute("nonce",n.nc),i.src=n.p+"static/js/"+e+"."+{0:"44590947aee345816232",1:"2a41b3969d7c77d3d1d0",2:"f7445158183346a80c49",3:"79228295982d3cceeb6d",4:"b9ae720f9ce2763fab1d",5:"72e581386c13ed03dbac",6:"0f7c11670e67ae378ba0",7:"ad55b2c17219a0ead069",8:"0101312770d9e6dad445"}[e]+".js";var u=setTimeout(r,12e4);return i.onerror=i.onload=r,c.appendChild(i),a},n.m=e,n.c=t,n.d=function(e,r,t){n.o(e,r)||Object.defineProperty(e,r,{configurable:!1,enumerable:!0,get:t})},n.n=function(e){var r=e&&e.__esModule?function(){return e.default}:function(){return e};return n.d(r,"a",r),r},n.o=function(e,n){return Object.prototype.hasOwnProperty.call(e,n)},n.p="/",n.oe=function(e){throw console.error(e),e}}([]);
+//# sourceMappingURL=manifest.834af170969952f72584.js.map
